99466881522a62d1739a88bcd86cc98d38d64c43
Healer <1977518916@qq.com>
2024-08-12 17:02:34 +0800

    feat；更新游戏场景,添加GameManager脚本

---
+++ Assets/Scripts/Runtime/Manager/GameManager.cs
@@ -0,0 +1,24 @@
+using Runtime.Data;
+using Tao_Framework.Core.Singleton;
+using UnityEngine;
+
+public class GameManager : MonoSingleton<GameManager>
+{
+    /// <summary>
+    /// 显示帧率
+    /// </summary>
+    public GameObject showFPS;
+    
+    private void Update()
+    {
+        
+    }
+    
+    /// <summary>
+    /// 是否显示帧率
+    /// </summary>
+    private void IsShowFPS()
+    {
+        
+    }
+}

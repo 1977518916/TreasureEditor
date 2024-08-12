9cb692d313c82ee18e54b479ce78df8cacd28d18
11955 <1195582817@qq.com>
2024-08-12 14:48:44 +0800

    增加资源加载管理类、数据管理类 完善英雄类型及对应UI显示

---
+++ Assets/Scripts/Runtime/Manager/AssetsLoadManager.cs
@@ -0,0 +1,25 @@
+﻿using Runtime.Data;
+using Spine.Unity;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Runtime.Manager
+{
+    public static class AssetsLoadManager
+    {
+        public static GameObject LoadHero(HeroType heroType)
+        {
+            string path = $"Character/{heroType.ToString()}/{heroType.ToString()}_SkeletonData";
+            SkeletonAnimation skeletonAnimation = LoadSkeletonAnimation(path);
+            skeletonAnimation.name = heroType.ToString();
+            return skeletonAnimation.GameObject();
+        }
+
+        public static SkeletonAnimation LoadSkeletonAnimation(string path)
+        {
+            SkeletonDataAsset asset = Resources.Load<SkeletonDataAsset>(path);
+            SkeletonAnimation skeletonAnimation = SkeletonAnimation.NewSkeletonAnimationGameObject(asset);
+            return skeletonAnimation;
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Manager/DataManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Runtime.Data;
+
+namespace Runtime.Manager
+{
+    public static class DataManager
+    {
+        public static readonly Dictionary<DataType.HeroPositionType, HeroData> HeroDatas = new Dictionary<DataType.HeroPositionType, HeroData>();
+        
+    }
+}

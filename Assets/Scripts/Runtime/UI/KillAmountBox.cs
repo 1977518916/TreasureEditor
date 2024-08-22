479fdc8b9be162fcb4ab782db802bc7509d4299b
cnoom <1195582817@qq.com>
2024-08-22 13:51:52 +0800

    增加敌人组装 粗略实现击杀UI计数

---
+++ Assets/Scripts/Runtime/UI/KillAmountBox.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Tao_Framework.Core.Event;
+using TMPro;
+using UnityEngine;
+
+namespace Runtime.UI
+{
+    public class KillAmountBox : MonoBehaviour
+    {
+        private TextMeshProUGUI amount;
+        private int currentAmount;
+        private EntitySystem entitySystem;
+        private void Awake()
+        {
+            amount = GetComponentInChildren<TextMeshProUGUI>();
+        }
+        private void Start()
+        {
+            entitySystem = EntitySystem.Instance;
+            amount.SetText(currentAmount.ToString());
+            EventMgr.Instance.RegisterEvent<int>(GameEvent.EntityDead,UpdateAmount);
+        }
+
+        private void UpdateAmount(int entityId)
+        {
+            
+            currentAmount++;
+            amount.SetText(currentAmount.ToString());
+        }
+    }
+}

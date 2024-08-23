108e1a98648e3f4b312b90c94ae6952b53c8be4c
cnoom <1195582817@qq.com>
2024-08-23 10:47:22 +0800

    运行时数据增加保存功能，增加游戏场景弹出

--- Assets/Scripts/Runtime/Manager/InputManager.cs
+++ Assets/Scripts/Runtime/Manager/InputManager.cs
@@ -6,13 +6,34 @@ namespace Runtime.Manager
 {
     public class InputManager : MonoSingleton<InputManager>
     {
-
+        public Canvas canvas;
+        private GameObject runDataUI;
         private void Update()
+        {
+            TryQuitBattle();
+            TryShowHideRunTimeData();
+        }
+
+        private void TryQuitBattle()
         {
             if(Input.GetKeyDown(KeyCode.Escape))
             {
                 BattleSceneManager.QuitBattle();
             }
+        }
+
+        private void TryShowHideRunTimeData()
+        {
+            if(!Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                return;
+            }
+            if(runDataUI == null)
+            {
+                runDataUI = Instantiate(AssetsLoadManager.Load<GameObject>("Prefabs/RunTimeSetting"), canvas.transform);
+                return;
+            }
+            runDataUI.SetActive(!runDataUI.activeSelf);
         }
     }
 }

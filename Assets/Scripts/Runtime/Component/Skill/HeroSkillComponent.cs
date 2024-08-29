b111106212b0d731c1642b3a68412ebf1eae5abf
cnoom <1195582817@qq.com>
2024-08-29 14:24:28 +0800

    技能系统提交

--- Assets/Scripts/Runtime/Component/Skill/HeroSkillComponent.cs
+++ Assets/Scripts/Runtime/Component/Skill/HeroSkillComponent.cs
@@ -3,56 +3,25 @@ using Runtime.System;
 using Runtime.Utils;
 using Tao_Framework.Core.Event;
 using UnityEngine;
+using NotImplementedException = System.NotImplementedException;
 
 namespace Runtime.Component.Skill
 {
     public class HeroSkillComponent : SkillComponent
     {
-        private HeroEntity entity;
-        private DataType.HeroPositionType positionType;
+        public HeroEntity entity;
+        public DataType.HeroPositionType positionType;
         public HeroSkillComponent(DataType.HeroPositionType positionType, HeroEntity heroEntity)
         {
             entity = heroEntity;
             this.positionType = positionType;
-            RegisterSkill();
         }
-
-        private void RegisterSkill()
-        {
-            EventMgr.Instance.RegisterEvent<KeyCode>(entity.EntityId, GameEvent.InvokeSkill, HandEvent);
-        }
-
+        
         public void Tick(float time)
         {
         }
         public void Release()
         {
-            EventMgr.Instance.RemoveEvent(entity.EntityId, GameEvent.InvokeSkill);
-        }
-
-        private void HandEvent(KeyCode key)
-        {
-            switch(key)
-            {
-                case KeyCode.Alpha0:
-                case KeyCode.Alpha1:
-                case KeyCode.Alpha2:
-                case KeyCode.Alpha3:
-                case KeyCode.Alpha4:
-                case KeyCode.Alpha5:
-                case KeyCode.Alpha6:
-                case KeyCode.Alpha7:
-                case KeyCode.Alpha8:
-                case KeyCode.Alpha9:
-                    int keyCodeValue = (int)key + (key == KeyCode.Alpha0 ? 10 : 0);
-                    int skillId = keyCodeValue - (int)positionType * 2 - (int)KeyCode.Alpha1 + 1;
-                    if(skillId is < 3 and > 0)
-                    {
-                        Debug.Log(TranslateUtil.TranslateUi(entity.GetHeroData().modelType) + "释放技能" + skillId);
-                        UseSkill(skillId);
-                    }
-                    break;
-            }
         }
 
         public void UseSkill(int id)

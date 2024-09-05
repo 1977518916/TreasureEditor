6eba7c7d253e06cc3ed46cbd699ee044ed96a696
cnoom <1195582817@qq.com>
2024-09-05 14:41:21 +0800

    添加qf框架对应文件夹 增加项目qf框架 初步重构优化UI代码

---
+++ Assets/Scripts/Runtime/Models/EnemiesDataModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using QFramework;
+using Runtime.Utils;
+
+namespace Runtime.Data
+{
+    /// <summary>
+    /// 所有波敌人的数据
+    /// </summary>
+    public class EnemiesDataModel : AbstractModel
+    {
+        private List<TimesData> timesData;
+        private List<TimesData> TimesData
+        {
+            get
+            {
+                return timesData ??= new List<TimesData>();
+            }
+            set
+            {
+                timesData = value;
+                this.GetUtility<ReadWriteUtility>().Write(nameof(EnemiesDataModel), timesData);
+            }
+        }
+        protected override void OnInit()
+        {
+            timesData = this.GetUtility<ReadWriteUtility>().Read(nameof(EnemiesDataModel), new List<TimesData>()
+            {
+                new TimesData()
+            });
+        }
+    }
+
+}
